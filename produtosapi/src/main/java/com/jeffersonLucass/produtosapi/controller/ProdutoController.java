fc04852a12eba653856d29914607b62860dd4c40
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-07-24 13:53:11 -0300

    Obtendos os dados do produto (Adição da requisição GETMAPPING)

--- produtosapi/src/main/java/com/jeffersonLucass/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/jeffersonLucass/produtosapi/controller/ProdutoController.java
@@ -2,18 +2,16 @@ package com.jeffersonLucass.produtosapi.controller;
 
 import com.jeffersonLucass.produtosapi.model.Produto;
 import com.jeffersonLucass.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("produtos")
+@RequestMapping("/produtos")
 public class ProdutoController {
 
-    private ProdutoRepository produtoRepository;
+    private final ProdutoRepository produtoRepository;
 
     public ProdutoController(ProdutoRepository produtoRepository) {
         this.produtoRepository = produtoRepository;
@@ -23,12 +21,19 @@ public class ProdutoController {
     @PostMapping
     public Produto Salvar(@RequestBody Produto produto){
         System.out.println("Produto recebido: " +  produto);
+        //gera um id aleatorio e transforma ele em uma string.
         var id = UUID.randomUUID().toString();
         produto.setId(id);
         produtoRepository.save(produto);
         return produto;
     }
+    //CAMINHO DA URL
+    @GetMapping("/{id}")
+    public Produto obterporId(@PathVariable("id") String id){
 
+        // Na linha abaixo se o id existir ele ira retorna-lo, caso não exista retornará nulo.
+        return produtoRepository.findById(id).orElse(null);
 
+    }
 
 }
