1bd3788425a4dcadb37a6cd30356496616d2c949
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-07-25 15:51:08 -0300

    Adicionando projeto produtosapi ao repositório principal

--- produtosapi/src/main/java/com/jeffersonLucass/produtosapi/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jeffersonLucass.produtosapi.repository;
-
-import com.jeffersonLucass.produtosapi.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto,String>{
-
-}
--- produtosapi/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-create table Produto (
-   id VARCHAR(255) NOT NULL PRIMARY KEY,
-   nome VARCHAR(50) NOT NULL,
-   descricao VARCHAR(300),
-   preco NUMERIC(18,2)
-);
