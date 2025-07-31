27bff86d6227c2dff7d5cf2ca708ed3a426a16ad
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-07-31 15:58:36 -0300

    Testando o fluxo através do postman

--- arquiteturaspring/src/main/java/io/github/jeffersonLucass/arquiteturaspring/Application.java
+++ arquiteturaspring/src/main/java/io/github/jeffersonLucass/arquiteturaspring/Application.java
@@ -10,24 +10,6 @@ import org.springframework.core.env.ConfigurableEnvironment;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-
-		//SpringApplication.run(Application.class, args);
-
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
-		builder.bannerMode(Banner.Mode.OFF);
-		builder.profiles("teste");
-		builder.run(args);
-
-
-		// Contexto da aplicação já iniciada:
-		ConfigurableApplicationContext Apllicationcontext = builder.context();
-
-
-		ConfigurableEnvironment environment = Apllicationcontext.getEnvironment();
-		String apllicationName = environment.getProperty("spring.application.name");
-		System.out.println("Nome da aplicação: " + apllicationName );
-
-	}
+	public static void main(String[] args) {SpringApplication.run(Application.class, args);}
 
 }
--- arquiteturaspring/src/main/java/io/github/jeffersonLucass/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/io/github/jeffersonLucass/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -4,9 +4,11 @@ import io.github.jeffersonLucass.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
     //UTILIZO ESSA ANNOTATION PARA INJETAR  A INSTANCIA DE MOTOR DA MONTADORACONFIGURATION.
 
