7967258107ce1cf4bedbd0147a646e1105f1c197
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-09-09 16:42:25 -0300

    Demonstrando a arquitetura MVC em funcionamento.

--- arquiteturaspring/src/main/java/io/github/jeffersonLucass/arquiteturaspring/todos/TodoService.java
+++ arquiteturaspring/src/main/java/io/github/jeffersonLucass/arquiteturaspring/todos/TodoService.java
@@ -16,4 +16,19 @@ public class TodoService {
         return repository.save(novoTodo);
 
     }
+
+    public void atualizarStatus(TodoEntity todo){
+
+        repository.save(todo);
+    }
+
+    public TodoEntity buscarPorId(Integer id){
+
+        return repository.findById(id).orElse(null);
+
+    }
+
+
+
+
 }
