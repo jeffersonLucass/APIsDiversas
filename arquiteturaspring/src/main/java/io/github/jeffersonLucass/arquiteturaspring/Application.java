46ca668dce89d9c12e2641d2bf1b38d30c862709
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-09-18 14:51:11 -0300

    Retirando imports inutilizados

--- arquiteturaspring/src/main/java/io/github/jeffersonLucass/arquiteturaspring/Application.java
+++ arquiteturaspring/src/main/java/io/github/jeffersonLucass/arquiteturaspring/Application.java
@@ -1,12 +1,8 @@
 package io.github.jeffersonLucass.arquiteturaspring;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @EnableConfigurationProperties
