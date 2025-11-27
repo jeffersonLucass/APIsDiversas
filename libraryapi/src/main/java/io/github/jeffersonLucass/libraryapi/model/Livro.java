24ba745992e123494a5607583d0be9028f8fc30b
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-11-27 16:08:25 -0300

    55. Como gerar as tabelas automaticamente com JPA

--- libraryapi/src/main/java/io/github/jeffersonLucass/libraryapi/model/Livro.java
+++ libraryapi/src/main/java/io/github/jeffersonLucass/libraryapi/model/Livro.java
@@ -3,6 +3,7 @@ package io.github.jeffersonLucass.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -29,7 +30,7 @@ public class Livro {
     private GeneroLivro genero;
 
     @Column(name = "preco", precision = 18, scale = 2)
-    private Double preco;
+    private BigDecimal preco;
     //private BigDecimal preco;
 
     //MUITOS LIVROS PARA UM AUTOR
