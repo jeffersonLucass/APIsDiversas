8284341e4cf7cbba02d93880ecd3b3dbe5136b1a
jeffersonLucass <jeffersonlucas810@gmail.com>
2026-03-14 08:38:00 -0300

    83.Obtendo detalhes de um Autor

--- libraryapi/src/main/java/io/github/jeffersonLucass/libraryapi/service/AutorService.java
+++ libraryapi/src/main/java/io/github/jeffersonLucass/libraryapi/service/AutorService.java
@@ -4,6 +4,9 @@ import io.github.jeffersonLucass.libraryapi.model.Autor;
 import io.github.jeffersonLucass.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class AutorService {
 
@@ -18,6 +21,8 @@ public class AutorService {
     }
 
 
+    public Optional<Autor> obterPorId(UUID id) {
+        return autorRepository.findById(id);
 
-
+    }
 }
